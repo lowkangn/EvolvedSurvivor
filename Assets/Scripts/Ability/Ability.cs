ed6496645ed069387cc99e7e9b48a73c0f1f833e
Ma-Yueran <mayuerantom@outlook.com>
2022-08-27 03:02:22 +0800

    update Ability

--- Assets/Scripts/Ability/Ability.cs
+++ Assets/Scripts/Ability/Ability.cs
@@ -2,53 +2,9 @@ using UnityEngine;
 using MoreMountains.Tools;
 using MoreMountains.TopDownEngine;
 
-public class Ability : MonoBehaviour
+public abstract class Ability : MonoBehaviour
 {
-    [SerializeField] private float abilityCooldown;
-    [SerializeField] private MMObjectPooler objectPool;
-    [SerializeField] private bool isLockOn;
-    [SerializeField] private bool isValidAbility; 
+    public bool Active { get; protected set; }
 
-    private float remainingCooldown;
-
-    private void Start()
-    {
-        remainingCooldown = abilityCooldown;
-    }
-
-    private void Update()
-    {
-        FireAbility();
-    }
-    
-    private void FireAbility()
-    {
-        if (!isValidAbility)
-        {
-            return;
-        } 
-        else
-        {
-            remainingCooldown -= Time.deltaTime;
-        }
-
-        if (remainingCooldown <= 0f)
-        {
-            GameObject nextGameObject = objectPool.GetPooledGameObject();
-            nextGameObject.transform.position = gameObject.transform.position;
-            nextGameObject.SetActive(true);
-
-            Projectile projectile = nextGameObject.GetComponent<Projectile>();
-            SetRandomDirection(projectile);
-
-            remainingCooldown = abilityCooldown;
-        }
-    }
-
-    private void SetRandomDirection(Projectile projectile)
-    {
-        projectile.SetDirection(
-            new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f),
-            transform.rotation);
-    }
+    protected abstract void ActivateAbility();
 }
