4998d224ffa0ff58b51f3074b1f6b235825c8d8e
Ma-Yueran <mayuerantom@outlook.com>
2022-08-27 03:33:59 +0800

    update ability structure

--- Assets/Scripts/Ability/Ability.cs
+++ Assets/Scripts/Ability/Ability.cs
@@ -2,9 +2,58 @@ using UnityEngine;
 using MoreMountains.Tools;
 using MoreMountains.TopDownEngine;
 
-public abstract class Ability : MonoBehaviour
+namespace TeamOne.EvolvedSurvivor
 {
-    public bool Active { get; protected set; }
+    public abstract class Ability : MonoBehaviour
+    {
+        [Header("Whether this ability will be activated while it is active")]
+        [SerializeField]
+        private bool activateOnlyOnce = false;
+        [Header("The ability is activated once every (coolDown) seconds")]
+        [SerializeField]
+        protected float coolDown;
 
-    protected abstract void ActivateAbility();
+        private bool hasBuilt = false;
+        private bool hasActivated;
+        private float coolDownTimer;
+
+        /// <summary>
+        /// Uses the score to (randomly) define the behaviours of the ability. 
+        /// E.g., Speed, Damage, CoolDown, etc.
+        /// MUST be called before the ability can be activated
+        /// </summary>
+        public void BuildAbility(int score)
+        {
+            Build(score);
+            hasBuilt = true;
+        }
+
+        private void Update()
+        {
+            if (!hasBuilt)
+            {
+                return;
+            }
+
+            if (activateOnlyOnce)
+            {
+                if (!hasActivated)
+                {
+                    Activate();
+                }
+                return;
+            }
+
+            coolDownTimer -= Time.deltaTime;
+            if (coolDownTimer < 0f)
+            {
+                Activate();
+                coolDownTimer = coolDown;
+            }
+        }
+
+        protected abstract void Build(int score);
+
+        protected abstract void Activate();
+    }
 }
