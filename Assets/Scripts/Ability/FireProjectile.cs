460623c4089be72283ffaa15d0c149edac843da2
Ma-Yueran <mayuerantom@outlook.com>
2022-08-27 03:34:11 +0800

    add fire projectile ability

---
+++ Assets/Scripts/Ability/FireProjectile.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TeamOne.EvolvedSurvivor
+{
+    public class FireProjectile : Ability
+    {
+        protected override void Activate()
+        {
+
+        }
+    }
+}
