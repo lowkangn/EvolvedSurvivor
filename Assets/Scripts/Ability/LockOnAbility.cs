4bff378c7c3e36602c988db59f6350cbad931b39
Stratostorm <stratostorm@outlook.com>
2022-09-20 15:48:16 +0800

    Initial creation of LockOn ability logic

--- Assets/Scripts/Ability/LockOnAbility.cs
+++ Assets/Scripts/Ability/LockOnAbility.cs
@@ -1,6 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using MoreMountains.TopDownEngine;
+using System.Collections.Generic;
 
 namespace TeamOne.EvolvedSurvivor
 {
@@ -15,7 +16,47 @@ namespace TeamOne.EvolvedSurvivor
 
         protected override void Activate()
         {
-            print("shoot");
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            GameObject[] onScreenEnemies = enemies.Where(x => IsOnScreen(x.transform)).ToArray();
+            HashSet<GameObject> enemiesHit = new HashSet<GameObject>();
+            if (onScreenEnemies.Length > 0)
+            {
+                // Shuffle the onScreenEnemies array
+                for (int n = onScreenEnemies.Length - 1; n > 0; n--)
+                {
+                    int k = Random.Range(0, n);
+                    GameObject temp = onScreenEnemies[n];
+                    onScreenEnemies[n] = onScreenEnemies[k];
+                    onScreenEnemies[k] = temp;
+                }
+                for (int i = 0; i < targetNumber.value && i < onScreenEnemies.Length; i++)
+                {
+                    // Damage chosen enemy
+                    GameObject chosenEnemy = onScreenEnemies[i];
+                    enemiesHit.Add(chosenEnemy);
+                    Collider2D[] enemiesInRadius = Physics2D.OverlapCircleAll(chosenEnemy.transform.position, aoeRadius.value, LayerMask.GetMask("Enemies"));
+                    foreach (Collider2D enemy in enemiesInRadius)
+                    {
+                        enemiesHit.Add(enemy.gameObject);
+                    }
+                }
+
+                // Iterate over hit enemies and damage all of them
+                foreach (GameObject enemy in enemiesHit)
+                {
+                    if (enemy.tag == "Enemy")
+                    {
+                        enemy.GetComponent<Health>().Damage(Mathf.RoundToInt(damage.value), gameObject, 0.5f, 0f, Vector3.zero);
+                    }
+                }
+            }
+        }
+
+        private bool IsOnScreen(Transform enemy)
+        {
+            Camera mainCamera = Camera.main;
+            Vector3 screenPoint = mainCamera.WorldToViewportPoint(enemy.transform.position);
+            return screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
         }
 
         protected override void Build()
