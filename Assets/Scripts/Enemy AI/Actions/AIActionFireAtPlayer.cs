fe40034a01c38f1b07aee11008e13f2bcd0aa561
lowkangn <kangngee62@gmail.com>
2022-09-26 19:43:16 +0800

    Add ranged enemy AI and attacks

--- Assets/Scripts/Enemy AI/Actions/AIActionFireAtPlayer.cs
+++ Assets/Scripts/Enemy AI/Actions/AIActionFireAtPlayer.cs
@@ -1,18 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
+using MoreMountains.Tools;
+using TeamOne.EvolvedSurvivor;
 using UnityEngine;
 
-public class AIActionFireAtPlayer : MonoBehaviour
+public class AIActionFireAtPlayer : AIAction
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private MMSimpleObjectPooler objectPool;
+    [SerializeField] private float timeForEachShot = 3f;
+
+    [SerializeField] private float damageValue = 10f;
+    [SerializeField] private float projectileSpeed = 10f;
+
+    private float timeSinceLastShot = 0f;
+    private Damage damage;
+
+    private void Start()
     {
-        
+        damage = new Damage();
+        damage.damage = damageValue;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        timeSinceLastShot += Time.deltaTime;
+    }
+
+    public override void PerformAction()
+    {
+        if (timeSinceLastShot >= timeForEachShot)
+        {
+            timeSinceLastShot = 0f;
+
+            FireProjectile();
+        }
+    }
+    
+    private void FireProjectile()
+    {
+        EnemyProjectile nextProjectile = objectPool
+                .GetPooledGameObject().GetComponent<EnemyProjectile>();
+
+        nextProjectile.transform.position = transform.position;
+        nextProjectile.SetDamage(damage);
+        nextProjectile.SetSpeed(projectileSpeed);
+
+        Vector3 directionToTarget = Vector3
+            .Normalize(_brain.Target.position - transform.position);
+        nextProjectile.SetDirection(directionToTarget);
+
+        nextProjectile.gameObject.SetActive(true);
     }
 }
