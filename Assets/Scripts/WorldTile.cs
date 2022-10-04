3ed4c41e5bc265d37801699d83c95e849d8988ff
lowkangn <kangngee62@gmail.com>
2022-10-04 22:22:46 +0800

    Add randomly generated background elements to the level

--- Assets/Scripts/WorldTile.cs
+++ Assets/Scripts/WorldTile.cs
@@ -1,15 +1,23 @@
+using MoreMountains.Tools;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class WorldTile : MonoBehaviour
 {
     [SerializeField] public int x;
     [SerializeField] public int y;
+    [SerializeField] private float cellSize = 1f;
+    [SerializeField] private float tileSize = 40f;
 
     private WorldScroller worldScroller;
+    private MMMultipleObjectPooler bgObjectPool;
 
-    private void Start()
+    private List<GameObject> bgObjects = new List<GameObject>();
+
+    private void Awake()
     {
         worldScroller = GetComponentInParent<WorldScroller>();
+        bgObjectPool = GetComponentInParent<MMMultipleObjectPooler>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -18,5 +26,36 @@ public class WorldTile : MonoBehaviour
         {
             worldScroller.UpdatePlayerPosition(x, y);
         }    
+    }
+
+    public void GenerateNewBackgroundObjects(float seed, float density)
+    {
+        foreach (GameObject bgObject in bgObjects)
+        {
+            bgObject.GetComponent<MMPoolableObject>().Destroy();
+            bgObjects = new List<GameObject>();
+        }
+
+        int tileSizeInCells = Mathf.FloorToInt(tileSize / cellSize);
+        Vector2 tilePosition = gameObject.transform.position;
+
+        for (int i = 0; i < tileSizeInCells; i++)
+        {
+            for (int j = 0; j < tileSizeInCells; j++)
+            {
+                float generatedNoise = Mathf.PerlinNoise((tilePosition.x + i) * 0.5f + seed, (tilePosition.y + j) * 0.5f + seed);
+
+                if (generatedNoise < density)
+                {
+                    GameObject bgObject = bgObjectPool.GetPooledGameObject();
+                    Vector2 offset = new Vector2((i + 0.5f) * cellSize, (j + 0.5f) * cellSize);
+                    bgObject.transform.position = tilePosition + offset;
+                    bgObject.transform.parent = gameObject.transform;
+                    bgObject.SetActive(true);
+
+                    bgObjects.Add(bgObject);
+                }
+            }
+        }
     }
 }
