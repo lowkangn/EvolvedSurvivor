96f7f84b3c522d834c0cd369692842bc53cdf0ce
Stratostorm <stratostorm@outlook.com>
2022-10-06 23:42:25 +0800

    rename variable

--- Assets/Scripts/Ability Handlers/ConeAbilityHandler.cs
+++ Assets/Scripts/Ability Handlers/ConeAbilityHandler.cs
@@ -5,7 +5,7 @@ namespace TeamOne.EvolvedSurvivor
     {
         private Orientation2D characterOrientation;
         [SerializeField]
-        private ParticleSystem particleSystem;
+        private ParticleSystem particles;
 
         private readonly float rateOverTime = 100f;
         private void OnEnable()
@@ -15,13 +15,13 @@ namespace TeamOne.EvolvedSurvivor
 
         public void UpdateParticles(float range, int coneNumber, float anglePerHalfCone)
         {
-            var main = particleSystem.main;
+            var main = particles.main;
             main.startSpeed = new ParticleSystem.MinMaxCurve(0.9f * range, 1.1f * range);
 
-            var emission = particleSystem.emission;
+            var emission = particles.emission;
             emission.rateOverTime = coneNumber * rateOverTime;
 
-            var shape = particleSystem.shape;
+            var shape = particles.shape;
             shape.angle = coneNumber * anglePerHalfCone;
         }
 
