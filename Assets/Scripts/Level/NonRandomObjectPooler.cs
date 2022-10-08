55fef434914d4388c52faa7996d5eed26b77349a
lowkangn <kangngee62@gmail.com>
2022-10-08 18:47:14 +0800

    Fix bug with points not being refunded correctly

--- Assets/Scripts/Level/NonRandomObjectPooler.cs
+++ Assets/Scripts/Level/NonRandomObjectPooler.cs
@@ -1,6 +1,7 @@
 using MoreMountains.Tools;
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NonRandomObjectPooler : MMMultipleObjectPooler
 {
@@ -13,6 +14,23 @@ public class NonRandomObjectPooler : MMMultipleObjectPooler
 
     public GameObject GetPooledObjectByName(string name)
     {
-        return GetPooledGameObjectOfType(name);
+        GameObject newObject = FindInactiveObject(name, _objectPool.PooledGameObjects);
+
+        if (newObject != null)
+        {
+            return newObject;
+        }
+        else
+        {
+            GameObject searchedObject = FindObject(name, _objectPool.PooledGameObjects);
+            GameObject newGameObject = (GameObject)Instantiate(searchedObject);
+
+            SceneManager.MoveGameObjectToScene(newGameObject, this.gameObject.scene);
+            newGameObject.transform.SetParent(_waitingPool.transform);
+            newGameObject.name = name;
+            _objectPool.PooledGameObjects.Add(newGameObject);
+
+            return newGameObject;
+        }
     }
 }
