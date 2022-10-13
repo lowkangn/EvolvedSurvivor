cf30daee1d52833a39da015423e02824e0cb85bf
ntwbruce <e0544240@u.nus.edu>
2022-10-13 10:25:53 +0800

    Create PassiveAbilityManager

---
+++ Assets/Scripts/Passives/PassiveAbilityManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TeamOne.EvolvedSurvivor
+{
+    public class PassiveAbilityManager : MonoBehaviour
+    {
+        public PassiveAbility[] abilities;
+    }
+}
