d019e18fb4105ec5ae60d8589742e188f1bc5371
Lauren Lee <e0424895@u.nus.edu>
2022-10-31 14:27:56 +0800

    Update help panel button scripts

--- Assets/Scripts/GUI/HelpBackButton.cs
+++ Assets/Scripts/GUI/HelpBackButton.cs
@@ -5,23 +5,28 @@ using UnityEngine.EventSystems;
 
 public class HelpBackButton : MonoBehaviour, IPointerClickHandler
 {
-    [SerializeField] private GameObject helpPage1;
-    [SerializeField] private GameObject helpPage2;
-    [SerializeField] private GameObject helpPage3;
+    [SerializeField] private GameObject[] helpPages;
 
     [SerializeField] private GameObject nextButton;
     [SerializeField] private GameObject backButton;
 
     public void OnPointerClick (PointerEventData eventData)
     {
-        if (helpPage2.activeSelf) { // Go from page 2 to 1
-            helpPage2.SetActive(false);
-            helpPage1.SetActive(true);
-            backButton.SetActive(false); // Set self to false if going to the first page
-        } else if (helpPage3.activeSelf) {
-            helpPage3.SetActive(false);
-            helpPage2.SetActive(true);
-            nextButton.SetActive(true); // Set nextButton to true if leaving the last page 
+        for (int i = 1; i < helpPages.Length; i++) {
+            if (helpPages[i].activeSelf) { // Go from page i to page i-1
+                helpPages[i].SetActive(false);
+                helpPages[i - 1].SetActive(true);
+
+                if (i - 1 == 0) {
+                    backButton.SetActive(false); // Set backButton to false if going to the first page
+                }
+
+                if (i == helpPages.Length - 1) {
+                    nextButton.SetActive(true); // Set nextButton to true if leaving the last page 
+                }
+
+                break;
+            }
         }
     }
 }
--- Assets/Scripts/GUI/HelpNextButton.cs
+++ Assets/Scripts/GUI/HelpNextButton.cs
@@ -5,23 +5,28 @@ using UnityEngine.EventSystems;
 
 public class HelpNextButton : MonoBehaviour, IPointerClickHandler
 {
-    [SerializeField] private GameObject helpPage1;
-    [SerializeField] private GameObject helpPage2;
-    [SerializeField] private GameObject helpPage3;
+    [SerializeField] private GameObject[] helpPages;
 
     [SerializeField] private GameObject nextButton;
     [SerializeField] private GameObject backButton;
 
     public void OnPointerClick (PointerEventData eventData)
     {
-        if (helpPage1.activeSelf) { // Go from Page 1 to 2
-            helpPage1.SetActive(false);
-            helpPage2.SetActive(true);
-            backButton.SetActive(true); // Set back to true if not on first page
-        } else if (helpPage2.activeSelf) {
-            helpPage2.SetActive(false);
-            helpPage3.SetActive(true);
-            nextButton.SetActive(false); // Set self to false if this is the last page
+        for (int i = 0; i < helpPages.Length - 1; i++) {
+            if (helpPages[i].activeSelf) { // Go from page i to page i+1
+                helpPages[i].SetActive(false);
+                helpPages[i + 1].SetActive(true);
+
+                if (i == 0) {
+                    backButton.SetActive(true); // Set backButton to true if not on first page
+                }
+                
+                if (i + 1 == helpPages.Length - 1) {
+                    nextButton.SetActive(false); // Set nextButton to false if this is the last page
+                }
+                
+                break;
+            }
         }
     }
 }
