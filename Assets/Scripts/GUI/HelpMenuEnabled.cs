0506bf33d170a82bf441a3e15fc5131005e1e89e
Lauren Lee <e0424895@u.nus.edu>
2022-11-04 14:47:01 +0800

    Update help panel buttons functions

--- Assets/Scripts/GUI/HelpMenuEnabled.cs
+++ Assets/Scripts/GUI/HelpMenuEnabled.cs
@@ -16,4 +16,42 @@ public class HelpMenuEnabled : MonoBehaviour
         nextButton.SetActive(true);
         backButton.SetActive(false);
     }
+
+    public void goToNextPage() {
+        for (int i = 0; i < helpPages.Length - 1; i++) {
+            if (helpPages[i].activeSelf) { // Go from page i to page i+1
+                helpPages[i].SetActive(false);
+                helpPages[i + 1].SetActive(true);
+
+                if (i == 0) {
+                    backButton.SetActive(true); // Set backButton to true if not on first page
+                }
+                
+                if (i + 1 == helpPages.Length - 1) {
+                    nextButton.SetActive(false); // Set nextButton to false if this is the last page
+                }
+                
+                break;
+            }
+        }
+    }
+
+    public void goToPrevPage() {
+        for (int i = 1; i < helpPages.Length; i++) {
+            if (helpPages[i].activeSelf) { // Go from page i to page i-1
+                helpPages[i].SetActive(false);
+                helpPages[i - 1].SetActive(true);
+
+                if (i - 1 == 0) {
+                    backButton.SetActive(false); // Set backButton to false if going to the first page
+                }
+
+                if (i == helpPages.Length - 1) {
+                    nextButton.SetActive(true); // Set nextButton to true if leaving the last page 
+                }
+
+                break;
+            }
+        }
+    }
 }
--- Assets/Scripts/GUI/HelpNextButton.cs
+++ Assets/Scripts/GUI/HelpNextButton.cs
@@ -5,30 +5,8 @@ using UnityEngine.EventSystems;
 
 public class HelpNextButton : UIButton
 {
-    [SerializeField] private GameObject[] helpPages;
-
-    [SerializeField] private GameObject nextButton;
-    [SerializeField] private GameObject backButton;
-
     public override void OnPointerClick(PointerEventData eventData)
     {
-        for (int i = 0; i < helpPages.Length - 1; i++) {
-            if (helpPages[i].activeSelf) { // Go from page i to page i+1
-                helpPages[i].SetActive(false);
-                helpPages[i + 1].SetActive(true);
-
-                if (i == 0) {
-                    backButton.SetActive(true); // Set backButton to true if not on first page
-                }
-                
-                if (i + 1 == helpPages.Length - 1) {
-                    nextButton.SetActive(false); // Set nextButton to false if this is the last page
-                }
-                
-                break;
-            }
-        }
-
         clickSfxHandler.PlaySfx();
     }
 }
