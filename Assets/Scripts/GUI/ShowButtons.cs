0057b9ecea677c7e74d93a2087fa2cd7c03f33b5
lowkangn <kangngee62@gmail.com>
2022-11-07 18:28:47 +0800

    Add ability to skip menu animation

--- Assets/Scripts/GUI/ShowButtons.cs
+++ Assets/Scripts/GUI/ShowButtons.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 public class ShowButtons : MonoBehaviour
 {
     [SerializeField] private List<GameObject> menuButtons;
+    [SerializeField] private Animator menuAnimator;
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            menuAnimator.Play("Loop");
+        }
+    }
 
     public void ShowMenuButtons()
     {
